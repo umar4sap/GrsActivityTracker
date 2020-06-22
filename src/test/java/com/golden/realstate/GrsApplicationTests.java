9f5d1283d3bafb3202b9a4ac87e49414a5f3f36f
umar4sap <umarsoft.java@gmail.com>
2020-06-22 10:58:57 +0400

    pending commit

--- src/test/java/com/golden/realstate/GrsApplicationTests.java
+++ src/test/java/com/golden/realstate/GrsApplicationTests.java
@@ -1,16 +1,21 @@
-package com.example.demo;
+package com.golden.realstate;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.golden.realstate.repository.GrsProjectRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GrsApplicationTests {
 
+	
 	@Test
 	public void contextLoads() {
+		
 	}
 
 }
